bcfd7ec4f1e8ec83cedf1edce29b6c277ea7f31c
KRTRYBUS <Krzysztof.Trybus@dbschenker.com>
2023-02-17 08:44:38 +0100

    day 4 update

--- day_3/match_case.py
+++ day_3/match_case.py
@@ -1,7 +1,7 @@
 todos = []
 
 while True:
-    user_action = input('Type add, show or exit: ')
+    user_action = input('Type add, show, edit or exit: ')
     user_action = user_action.strip().lower()
 
     match user_action:
@@ -15,6 +15,16 @@ while True:
             else:
                 for item in todos:
                     print(item)
+        case 'edit':
+            i = 1
+            for item in todos:
+                print(f'{i} - {item}')
+                i += 1
+
+            edit_element_index = int(input('Please choose number to edit from list above: '))
+            edit_element_index = edit_element_index - 1
+            edit_element_value = input(f"Please provide new value for '{todos[edit_element_index]}': ")
+            todos[edit_element_index] = edit_element_value.title()
         case 'exit':
             break
         case _:
