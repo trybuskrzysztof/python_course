377f1fac5f9a9027b210ef9212904a94fe7183cc
KRTRYBUS <Krzysztof.Trybus@dbschenker.com>
2023-02-20 14:48:58 +0100

    day 6 update

--- todo_list.py
+++ todo_list.py
@@ -1,20 +1,26 @@
-todos = []
-
 while True:
-    user_action = input('Type add, show, edit or exit: ')
+    user_action = input('Type add, show, edit, complete or exit: ')
     user_action = user_action.strip().lower()
 
     match user_action:
         case 'add':
-            todo = input('Enter a todo: ')
-            todos.append(todo.title())
+            todo = input('Enter a todo: ') + "\n"
+
+            file = open('todos.txt', 'r')
+            todos = file.readlines()
+
+            todos.append(todo)
             todos.sort()
+            file = open('todos.txt', 'w')
+            file.writelines(todos)
+            file.close()
         case 'show' | 'display':
             if len(todos) == 0:
                 print('List contains no items')
             else:
-                for item in todos:
-                    print(item)
+                for item_index, item in enumerate(todos, start=1):
+                    row = f"{item_index}-{item}"
+                    print(row)
         case 'edit':
             i = 1
             for item in todos:
@@ -25,6 +31,9 @@ while True:
             edit_element_index = edit_element_index - 1
             edit_element_value = input(f"Please provide new value for '{todos[edit_element_index]}': ")
             todos[edit_element_index] = edit_element_value.title()
+        case 'complete':
+            number = int(input('Please choose number of item to complete: '))
+            todos.pop(number - 1)
         case 'exit':
             break
         case _:
