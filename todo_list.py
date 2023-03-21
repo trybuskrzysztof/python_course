034e690cb4546ac821d028e34ba7e116ca631995
KRTRYBUS <Krzysztof.Trybus@dbschenker.com>
2023-03-21 20:35:11 +0100

    day 11 update

--- todo_list.py
+++ todo_list.py
@@ -1,14 +1,18 @@
 file_dir = 'files/todos.txt'
 
+def get_todos():
+    with open(file_dir, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
 while True:
     user_action = input('Type add, show, edit, complete or exit: ')
     user_action = user_action.strip().lower()
 
     if user_action.startswith('add'):
-        todo = user_action[4:] + "\n"
+        todo = user_action[4:]
 
-        with open(file_dir, 'r') as file:
-            todos = file.readlines()
+        todos = get_todos()
 
         todos.append(todo + '\n')
 
@@ -16,8 +20,7 @@ while True:
             file.writelines(todos)
 
     elif user_action.startswith('show'):
-        with open(file_dir, 'r') as file:
-            todos = file.readlines()
+        todos = get_todos()
 
         # new_todos = [item.strip('\n') for item in todos]
 
@@ -34,8 +37,7 @@ while True:
             number = int(user_action[5:])
             number = number - 1
 
-            with open(file_dir, 'r') as file:
-                todos = file.readlines()
+            todos = get_todos()
 
             new_todo = input("Enter new todo: ")
             todos[number] = new_todo + '\n'
@@ -50,8 +52,8 @@ while True:
         try:
             number = int(user_action[9:])
 
-            with open(file_dir, 'r') as file:
-                todos = file.readlines()
+            todos = get_todos()
+
             index = number - 1
             todo_to_remove = todos[index].strip('\n')
             todos.pop(index)
